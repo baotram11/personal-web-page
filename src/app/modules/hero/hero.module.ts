9fb6bc5b11665ae1a28a853ba3e5220298c681a8
baotram11 <19127588@student.hcmus.edu.vn>
2022-09-01 21:56:40 +0700

    Project Initiation: Setup Project Environment

---
+++ src/app/modules/hero/hero.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroComponent } from './hero.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeroComponent]
+})
+export class HeroModule { }
---
+++ src/app/shared/pipes/pipes.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PipesPipe } from './pipes.pipe';
+
+describe('Pipe: Pipese', () => {
+  it('create an instance', () => {
+    let pipe = new PipesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
